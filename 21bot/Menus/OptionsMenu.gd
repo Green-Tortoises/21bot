a7ddb07b29d5f3e7939e75a1f6c05fac2f66bc60
GustavoLR548 <gustavolr035@gmail.com>
2022-06-06 15:37:27 -0300

    feat: adicionado opcoes de som

--- 21bot/Menus/OptionsMenu.gd
+++ 21bot/Menus/OptionsMenu.gd
@@ -11,7 +11,7 @@ var settings         = GameSettings
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
-	card_dealing_label.text = "   Card Dealing Timer: " + str(settings.card_timer)
+	card_dealing_label.text = "   Card Dealing Timer: " + str(settings.card_timer) + " seconds"
 	card_dealing.value      = settings.card_timer
 	
 	for model in model_controller.AiModels:
@@ -30,4 +30,4 @@ func _on_ModelSelection_item_selected(index):
 
 func _on_CardDealingTimer_value_changed(value):
 	settings.card_timer     = value
-	card_dealing_label.text = "   Card Dealing Timer: " + str(settings.card_timer)
+	card_dealing_label.text = "   Card Dealing Timer: " + str(settings.card_timer) + " seconds"
